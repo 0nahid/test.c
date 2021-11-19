0d9a094472662dca0ee55eb58472fb06112199a6
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-11-19 19:55:50 +0600

    .idea added from CLion :

--- leapYear.c
+++ leapYear.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <conio.h>
 
-int yearr(int year);
-void main(void){
+int checkYear(int year);
+int main(void){
     int year;
     printf("Enter year : ");
     scanf("%d",&year);
     (((year & 3) == 0 && (year % 25) != 0) || (year & 15) == 0) ? printf("This is a leap year") :printf("It's not a leap year") ;
     getch();
 }
-int yearr(int year) {
+int checkYear(int year) {
     ((year % 4 == 0) && (year / 4 != 0))?  1 :  0;
 }
