ee29fa067e11423472cea3ea170c150896846489
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-11-20 11:38:49 +0600

    added year validation

--- leapYear.c
+++ leapYear.c
@@ -2,13 +2,14 @@
 
 #include <stdio.h>
 #include <conio.h>
+#include<math.h>
 
 int checkYear(int year);
 int main(void){
     int year;
     printf("Enter year : ");
     scanf("%d",&year);
-    (((year & 3) == 0 && (year % 25) != 0) || (year & 15) == 0) ? printf("This is a leap year") :printf("It's not a leap year") ;
+    (((year & 3) == 0 && (year % 25) != 0) || (year & 15) == 0) && ( fmodf(year,1.0) != 0) ? printf("This is a leap year") :printf("It's not a leap year") ;
     getch();
 }
 int checkYear(int year) {
